df4c22c07bc3de54f2e0a04b70f9afab6b71d1a1
Christabel Sebastian <christabel.escarez@gmail.com>
2021-01-06 16:31:31 -0800

    latest line outputs under "most recent submission"

--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -6,7 +6,7 @@ const RecentSubmission = ({submission}) => {
   return (
     <div className="RecentSubmission">
       <h3>The Most Recent Submission</h3>
-      <p className="RecentSubmission__submission">{  }</p>
+      <p className="RecentSubmission__submission">{submission}</p>
     </div>
   );
 }
